83836574744c1e2df3c07beb3b1fd1ec3d4c0df0
Hugh Kennedy <hughskennedy@gmail.com>
2015-03-15 12:40:38 -0700

    formatting

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 var debounce = require('frame-debounce')
 var fit      = require('canvas-fit')
 
-var editor = require('./editor')(document.body, [
-  'precision mediump float;',
-  '',
-  'uniform float iGlobalTime;',
-  'uniform vec3 iResolution;',
-  '',
-  '#pragma glslify: noise = require("glsl-noise/simplex/3d")',
-  '',
-  'void main() {',
-  '  gl_FragColor.rgb = vec3(noise(vec3(gl_FragCoord.xy * 0.01, iGlobalTime * 0.001)));',
-  '  gl_FragColor.a = 1.0;',
-  '}'
-].join('\n'))
+var editor = require('./editor')(document.body, `precision mediump float;
+
+uniform float iGlobalTime;
+uniform vec3  iResolution;
+
+#pragma glslify: noise = require("glsl-noise/simplex/3d")
+
+void main() {
+  float n = noise(vec3(gl_FragCoord.xy, iGlobalTime * 0.1) * 0.005);
+  gl_FragColor.rgb = vec3(n);
+  gl_FragColor.a   = 1.0;
+}
+
+`)
 
 var canvas = document.querySelector('canvas')
 var display = require('./display')(canvas)
